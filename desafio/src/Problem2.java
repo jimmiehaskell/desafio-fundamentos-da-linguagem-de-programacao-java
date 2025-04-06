9b08cb1b941d3c947df2147023e226255ccfcf60
Jimmie Haskell <haskell4228@gmail.com>
2025-04-06 17:03:02 -0300

    desafio 2

---
+++ desafio/src/Problem2.java
@@ -0,0 +1,48 @@
+/*
+ * O código para solução do problema no site da dio era:
+ * 
+    import java.util.Scanner;
+
+    public class Problem {
+
+        public static void main(String[] args) {
+            Scanner sc = new Scanner (System.in);
+    
+            int A, B, soma;
+    
+            A = sc.nextInt();
+            B = sc.nextInt();
+    //TODO: Complete os espaços em branco com uma possível solução para o desafio 
+            soma = A+B;  
+    
+            System.out.println("SOMA = " + soma);
+        }
+    }
+ *
+ * Resolvi fazer de modo diferente para fixar o aprendizado
+ * incluíndo a possibilidade de interação com o usário e a
+ * possibilidade de valores diferentes.
+ */
+
+import java.util.Scanner;
+
+public class Problem2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int numberOne;
+        int numberTwo;
+        int soma;
+
+        System.out.println("Enter the first number:");
+        numberOne = scanner.nextInt();
+
+        System.out.println("Enter the secound number:");
+        numberTwo = scanner.nextInt();
+
+        soma = numberOne + numberTwo;
+
+        System.out.println("SOMA = " + soma);
+        scanner.close();
+    }
+}
